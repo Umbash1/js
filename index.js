168e67ad0d3cc41e7295811ae24b676f3181654c
Umbash1 <146041363+Umbash1@users.noreply.github.com>
2023-10-03 10:52:21 +0000

    new Amazon

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello world ");
+let box = document.querySelector("div")
+input = document.querySelector ("input")
+
+input.addEventListener ("input",()=>{
+
+box.style.borderRadius = input.value;
+box.style.background = input.value
+
+}
+
+)
